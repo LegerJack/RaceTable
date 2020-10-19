9653715a95f42d9c58e85ccad13a706b9b5c1e8d
Влад Кленов <Vlad.klenov@live.ru>
2020-10-20 01:26:37 +0500

    sorting is almost ready

--- index.php
+++ index.php
@@ -15,60 +15,88 @@ $place = 1;
 
 <body>
     <table class="dintable" id="raceTable">
+        <thead>
+            <tr class="dinthead">
+                <th>Место</th>
+                <th>Name</th>
+                <th>City</th>
+                <th>Car</th>
+                <? for($i = 1; $i <= $attempts; $i++): ?>
 
-        <tr class="dinthead">
-            <th>Место</th>
-            <th>Name</th>
-            <th>City</th>
-            <th>Car</th>
-            <? for($i = 1; $i <= $attempts; $i++): ?>
+                <th class="result" onclick="sortResult('<? echo $i ?>')">Result #
+                    <?echo $i?>
+                </th>
 
-            <th class="result" >Result #
-                <?echo $i?>
-            </th>
+                <?endfor;?>
+                <th id="total">Total</th>
 
-            <?endfor;?>
-            <th id="total">Total</th>
-
+            </tr>
+        </thead>
+        <tbody id="tbody">
             <?foreach ($drivertotal as $totaldata):?>
-        </tr>
+            <tr>
+                <td>
+                    <?echo $place++?>
+                </td>
+                <td>
+                    <?echo $totaldata['name']?>
+                </td>
+                <td>
+                    <?echo $totaldata['city']?>
+                </td>
+                <td>
+                    <?echo $totaldata['car']?>
+                </td>
 
-        <tr>
-            <td>
-                <?echo $place++?>
-            </td>
-            <td>
-                <?echo $totaldata['name']?>
-            </td>
-            <td>
-                <?echo $totaldata['city']?>
-            </td>
-            <td>
-                <?echo $totaldata['car']?>
-            </td>
+                <? for($i = 0; $i < $attempts; $i++ ): ?>
 
-            <? for($i = 0; $i < $attempts; $i++ ): ?>
+                <td onclick="">
+                    <?echo $totaldata['results'][$i]?>
+                </td>
 
-            <td onclick="">
-                <?echo $totaldata['results'][$i]?>
-            </td>
+                <? endfor; ?>
 
-            <? endfor; ?>
+                <td onclick="sortTotal('<?$drivertotal?>')">
+                    <?echo $totaldata['total']?>
+                </td>
 
-            <td onclick="sortTotal('<?$drivertotal?>')">
-                <?echo $totaldata['total']?>
-            </td>
-
-        </tr>
-        <?endforeach;?>
+            </tr>
+            <?endforeach;?>
+        </tbody>
     </table>
 
-    <script src="script.js"></script>
     <script>
-        let arr = <?echo json_encode($drivertotal);?>;
-        arr.sort((a,b) => a['results'][1] - b['results'][1]);
-        
-        console.log(arr);
+        function sortResult(e) {
+            let arr = <? echo json_encode($drivertotal); ?> ;
+            arr.sort((a, b) => a['results'][e] - b['results'][e]);
+            let tbody = document.getElementById('tbody');
+            while (tbody.firstChild) {
+                tbody.removeChild(tbody.firstChild);
+            }
+            for (i = 0; i < arr.length - 1; i++) {
+                let trow = document.createElement('tr');
+                tbody.appendChild(trow);
+                arrKeys = Object.keys(arr[i]);
+                k = 0;
+                for (j = 0; j < arrKeys.length + <? echo $attempts ?> ; j++) {
+                    let td = document.createElement('td');
+                    if(arrKeys[j] == "results"){
+                        td.appendChild(document.createTextNode('' + arr[i]['results'][k]));
+                        k++;
+                    }
+                    else{
+                        td.appendChild(document.createTextNode('' + arr[i][arrKeys[j]]));
+                        
+                    }
+
+                    trow.appendChild(td)
+                }
+            }
+            console.log(Object.keys(arr[1]).length);
+            console.log(Object.values(arr[1]['results']));
+            console.log(arr);
+            console.log(arrKeys);
+        }
     </script>
 </body>
 
