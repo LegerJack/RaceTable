806d56d21a82dc3eac06eaf581ffbcade4d477a8
Влад Кленов <Vlad.klenov@live.ru>
2020-10-20 20:03:19 +0500

    updated code, add comment, project is DONEgit add .!

--- JSON_processing.php
+++ JSON_processing.php
@@ -1,22 +1,35 @@
 <?php
+/**
+ * Читаем содержимое в JSON файлах
+ */
 $results = file_get_contents('JSON_db\data_attempts.json');
 $drivers = file_get_contents('JSON_db\data_cars.json');
-
+/**
+ * Декодируем JSON.
+ * Зписываем в переменную для дальнейших 
+ * действий.
+ */
 $dataresult = json_decode($results, true);
 $datadriver = json_decode($drivers, true);
-
+/**
+ * Из переменных с данными из JSON 
+ * Сравниваем данные и заиписываем их в единый многомерный массив 
+ */
 $i = 0;
 foreach ($datadriver as $driver) {
-    $attempts = 0;
+    $attempts = 0; //Переменная для подсчета количества поыток участников гонки
     $results = array();
     foreach ($dataresult as $result) {
         if ($driver['id'] == $result['id']) {
-            $total += $result['result'];
+            $total += $result['result'];     //Считаем общее количество очков участника
             $attempts++;
-            $results[$i][] = $result['result'];
+            $results[$i][] = $result['result']; //Отдельно записываем набранные очки в каждой попытке
         }
     };
 
+ /**
+  * Заносим полученные данные в массив для его дальнейшей сортировки 
+  */
     $drivertotal[$i] = array(
         'id' => $driver['id'],
         'name' => $driver['name'],
@@ -26,13 +39,18 @@ foreach ($datadriver as $driver) {
         'total' => $total
     );
     $i++;
-    $total = 0;
+    $total = 0; //Обнуление переменной для следующего подсчета очков участника
 };
+/**
+ * Функция для сортировки массива.
+ * Узнаем у кого больше всего очков.
+ */
 function thisFirst($a,$b){
     
     return ($b['total'] - $a['total']);
 }
+/**
+ * Сортируем массив
+ */
 usort($drivertotal, "thisFirst");
 
-// var_dump($drivertotal);
-// print_r($attempts);
--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?
-include_once 'JSON_processing.php';
-$place = 1;
+<!-- Подключаем файл с обработанными данными -->
+<?include_once 'JSON_processing.php';?>
 
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,7 +20,8 @@ $place = 1;
                 <th>Name</th>
                 <th>Car</th>
                 <th>City</th>
-                <? for($i = 1; $i <= $attempts; $i++): ?>
+                <? $position = 1; //Переменная для отображения места учстника 
+                 for($i = 1; $i <= $attempts; $i++): ?>
 
                 <th class="result" onclick="sortResult('<? echo $i ?>')">Result #
                     <?echo $i?>
@@ -34,10 +33,33 @@ $place = 1;
             </tr>
         </thead>
         <tbody id="tbody">
-            <?foreach ($drivertotal as $totaldata):?>
-            <tr>
+            
+            <?/**
+             * Выводим участников из массива в таблицу
+             */
+            foreach ($drivertotal as $totaldata):?>
+
+            <tr 
+            <? /**
+             * В зависимости от места выводим стиль строки
+             * 1 - золотая строка
+             * 2 - серебряная
+             * 3 - бронзовая
+             */
+            switch($position): 
+                case 1: ?>
+                    class="firstPOS"
+                <?break;?>
+                <? case 2: ?>
+                    class="secondPOS"; 
+                <?break;?>
+                <? case 3: ?> 
+                    class="thirdPOS"
+                <?break;?>
+                <? endswitch; ?> 
+            >
                 <td>
-                    <?echo $place++?>
+                    <?echo $position++?>
                 </td>
                 <td>
                     <?echo $totaldata['id']?>
@@ -52,7 +74,12 @@ $place = 1;
                     <?echo $totaldata['city']?>
                 </td>
 
-                <? for($i = 0; $i < $attempts; $i++ ): ?>
+                <? 
+                /**
+                 * Выводим результаты каждой попытки
+                 * и общего результата 
+                 */
+                for($i = 0; $i < $attempts; $i++ ): ?>
 
                 <td onclick="">
                     <?echo $totaldata['results'][$i]?>
@@ -71,9 +98,17 @@ $place = 1;
 
     <script>
         let attempts = <? echo $attempts; ?>;
+        /**
+         * Функция вызывающаяся при клике.
+         * Сортирует таблицу по всем результатам, как по отдельным ,так и по общему.
+         * */
         function sortResult(e) {
+            //Записываем объект JSON 
             let driverObj = <? echo json_encode($drivertotal); ?> ;
-
+            /**
+             * Сортируем объект по полученному элементу
+             * e - число указывающее на попытку по которой будет сортироваться массив  
+             */
             if(e <= attempts){
                 driverObj.sort((a, b) => b.results[e-1] - a.results[e-1]);
             }
@@ -82,32 +117,67 @@ $place = 1;
                 driverObj.sort((a, b) => b.total - a.total);
             }
 
+            /**
+             * Из тела таблицы удаляем ВСЕ имеющиеся записи для их переопрделения 
+             */
             let tbody = document.getElementById('tbody');
 
             while (tbody.firstChild) {
                 tbody.removeChild(tbody.firstChild);
             }
 
+            /**
+             * Добавлем элементы таблицы и записываем в них
+             * данные отсортированного массива
+             */
             for (i = 0; i < driverObj.length; i++) {
                 let trow = document.createElement('tr');
+            /**
+             * В зависимости от места добавляем стиль строки
+             * 1 - золотая строка
+             * 2 - серебряная
+             * 3 - бронзовая
+             */
                 tbody.appendChild(trow);
+                switch(i){
+                    case 0:{
+                        trow.classList.add('firstPOS');
+                        break;
+                    };
+                    case 1:{
+                        trow.classList.add('secondPOS');
+                        break;
+                    };
+                    case 2:{
+                        trow.classList.add('thirdPOS');
+                        break;
+                    };
+                }
+                /**
+                 * Получаем ключи объекта.
+                 * По ним определяем количество столбцов в строке.
+                 */
                 arrKeys = Object.keys(driverObj[i]);
 
+                //Создаем первую ячейку для просмотра занятого участником места
                 let td = document.createElement('td');
                 td.appendChild(document.createTextNode(i+1));
                 trow.appendChild(td);
-
+                //Получаем все остальные ячейки с данными
                 for (j = 0; j < arrKeys.length; j++) {
-                    
+                    //Если появляется ключ с результатами
+                    //Просматриваем просматриваем его
+                    //И выводим в таблицу
+                    //Выводим результаты каждой поптыки участника
                     if(arrKeys[j] == "results"){
                         for(k = 0; k < attempts; k++){
                             td = document.createElement('td');
                             td.appendChild(document.createTextNode(driverObj[i][arrKeys[j]][k] + " "));
-
                             trow.appendChild(td);
                         }
                     }
 
+                    //Иначе выводим следующие ячейки с данными и общим результатом участника
                     else {
                         td = document.createElement('td');
                         td.appendChild(document.createTextNode('' + driverObj[i][arrKeys[j]]));
